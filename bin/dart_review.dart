d51b3649ffe4e09b01f5d58d45152013d4bef3c4
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-06-18 22:31:34 +0300

    input in dart

--- bin/dart_review.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-////Getting Input in dart
-void main(){
-  print("Enter Your Year of Birth");
-  var yearOfBirth = stdin.readLineSync();
-  var currentYear = DateTime.now().year;
-  print("Your Age is ${currentYear-num.parse(yearOfBirth!)} Years Old");
-}
